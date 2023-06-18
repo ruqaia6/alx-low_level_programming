32c1bb6a4fa6ca3bf23b02a541b36be3f020393c
ruqaia6 <rqyhm755@gmail.com>
2023-06-18 06:37:10 -0700

    ff

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print all alphabet lowercase
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+       char ch = 'a';
+       char CH = 'A';
+
+       /*print a - z*/
+       while (ch <= 'z')
+       {
+               putchar(ch);
+               ch++;
+       }
+
+       /*print A - Z*/
+       while (CH <= 'z')
+       {
+               putchar('CH');
+               CH++;
+       }
+       putchar('\n');
+
+       return (0);
+}
