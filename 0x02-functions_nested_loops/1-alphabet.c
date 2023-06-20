f2d2ddfed7dfd088e3e1c811eba7193dc18f8751
ruqaia6 <rqyhm755@gmail.com>
2023-06-20 13:38:36 -0700

    ff

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+int ch;
+for (ch = 'a' : ch <= 'z' : ch++)
+_putchar('\n');
