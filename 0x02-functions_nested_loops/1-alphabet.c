d5b9ec4fa4bd22d735c2d4a71d6a19ef35db0add
ruqaia6 <rqyhm755@gmail.com>
2023-06-20 14:42:59 -0700

    gg

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-*print_alphabet_x10 - function that will print alphabet 10 times
+*Describtion: print_alphabet - utilizes on the _putchar funcation to print
+the alphabet a - z
 *
 */
 
@@ -10,8 +11,7 @@ void print_alphabet_x10(void)
 int ch;
 
 for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-_putchar(c);
-}
+
+_putchar(ch);
 _putchar('\n');
 }
