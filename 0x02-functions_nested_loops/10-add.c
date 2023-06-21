6cbf881a4cd0cf2809e7e6b7c5c1a510170044c7
ruqaia6 <rqyhm755@gmail.com>
2023-06-21 09:48:41 -0700

    lll

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,15 +1,15 @@
 #include "main.h" 
  
 /** 
- * mul - multiplies two integers 
+ * add - add two integers 
  * 
  * @a: first input 
  * @b: second input 
  * 
- * Return: return results 
+ * Return:sum of a & b 
 */ 
  
 int mul(int a, int b) 
 { 
-return (a * b); 
+return (sum); 
 }
