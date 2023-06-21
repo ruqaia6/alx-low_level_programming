96a09058c1ddb40a0ce30e1e2a54e1533c2dc235
ruqaia6 <rqyhm755@gmail.com>
2023-06-21 06:36:18 -0700

    ff

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table
+ */
+void times_table(void)
+{
+int i, j;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+if (j == 0)
+printf("%d", i * j);
+else if (i * j < 10)
+printf(",   %d", i * j);
+else
+printf(", %d", i * j);
+}
+putchar('\n');
+}
+}
