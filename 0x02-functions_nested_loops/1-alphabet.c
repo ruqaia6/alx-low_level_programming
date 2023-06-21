8ef358cf79ee4a67f6469c30bc6d5898850cf5cb
ruqaia6 <rqyhm755@gmail.com>
2023-06-21 03:55:55 -0700

    ff

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,29 +1,12 @@
-#include <stdio.h>
 #include "main.h"
 
-void print_alphabet(void);
-
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-/* Call the print_alphabet function */
-print_alphabet();
-
-/* Return 0 to indicate successful execution */
-return (0);
-}
-
-/**
-* print_alphabet - Prints the alphabet in lowercase followed by a new line
-*/
-void print_alphabet(void)
-{
-int ch;
-
-/* Loop through each character in the alphabet */
-for (ch = 'a' ; ch <= 'z' ; ch++)
-_putchar(ch);
-
-/* Print a new line character */
-_putchar('\n');
-return;
+	print_alphabet();
+	return (0);
 }
