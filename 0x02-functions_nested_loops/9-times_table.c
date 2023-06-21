91c5ba98339c7863c81a167f56a5d52e38122f11
ruqaia6 <rqyhm755@gmail.com>
2023-06-21 07:31:22 -0700

    fff

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,7 +3,6 @@
 /**
  * times_table - Prints the 9 times table
  */
-
 void times_table(void)
 {
 int i, j;
@@ -14,10 +13,10 @@ for (j = 0; j <= 9; j++)
 {
 if (i * j < 10)
 printf("%d", i * j);
-else if (i * j < 10)
-printf(",   %d", i * j);
-else
+else if (i * j >=10)
 printf(", %d", i * j);
+if (j !=9)
+printf(", ");
 }
 _putchar('\n');
 }
