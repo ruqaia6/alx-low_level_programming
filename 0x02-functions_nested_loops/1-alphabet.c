13a68dab3f64a8766b7c606f8740a15c954263d4
ruqaia6 <rqyhm755@gmail.com>
2023-06-21 04:12:05 -0700

    ff

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,19 @@
 #include "main.h"
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
+void print_alphabet(void);
+
 {
-	print_alphabet();
-	return (0);
+print_alphabet();
+return (0);
+}
+
+void print_alphabet(void)
+{
+int ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+_putchar(ch);
+_putchar('\n');
+
+return;
 }
