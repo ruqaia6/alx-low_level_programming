fa5f64d40c63aaf8d8ab0418d2f319e0508d592e
ruqaia6 <rqyhm755@gmail.com>
2023-06-22 13:58:45 -0700

    kk

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- *
  * _isupper - check if c is upper
  *
  * @c: input for alphabet
@@ -11,12 +10,12 @@
 
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-{
-return (1);
-}
-else
-{
-return (0);
-}
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
