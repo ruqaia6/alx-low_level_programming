1f57006155be6133815e3cffc6c46919b9b69c7d
ruqaia6 <rqyhm755@gmail.com>
2023-06-22 07:59:38 -0700

    dddd

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,40 +1,41 @@
-#include+$\e"main.h"
-/**+$\e
-*+$\eprint_remaining_days - takes a date and prints how many days are 
-*+$\eleft in the year, taking leap years into account 
-*
-*+$\e@month: month in number format
-*+$\e@day: day of month
-*+$\e@year: year
-*/
-
-void+$\eprint_remaining_days(int month, int day, int year)
-{
-/**
-*+$\eleap year when it can be divided
-*+$\eby 100 and 400 or by 4 evenly
-*/
-if+$\e((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
-{
-if+$\e(month > 2 && day >= 60)
-{
-day++;
-}
-
-printf("Day+$\eof the year: %d\n", day);
-printf("Remaining+$\edays: %d\n", 366 - day);
-
-}
-else
-{
-if+$\e(month == 2 && day == 60)
-{
-printf("Invalid+$\edate: %02d/%02d/%04d\n", month, day - 31, year);
-}
-else
-{
-printf("Day+$\eof the year: %d\n", day);
-printf("Remaining+$\edays: %d\n", 365 - day);
-}
-}
+#include "main.h" 
+ 
+/** 
+* print_remaining_days - takes a date and prints how many days are 
+*  left in the year, taking leap years into account 
+* 
+* @month: month in number format 
+* @day: day of month 
+* @year: year 
+*/ 
+ 
+void print_remaining_days(int month, int day, int year) 
+{ 
+ /** 
+  * leap year when it can be divided 
+  * by 100 and 400 or by 4 evenly 
+ */ 
+ if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0)) 
+ { 
+  if (month > 2 && day >= 60) 
+  { 
+   day++; 
+  } 
+ 
+  printf("Day of the year: %d\n", day); 
+  printf("Remaining days: %d\n", 366 - day); 
+ 
+ } 
+ else 
+ { 
+  if (month == 2 && day == 60) 
+  { 
+   printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year); 
+  } 
+  else 
+  { 
+   printf("Day of the year: %d\n", day); 
+   printf("Remaining days: %d\n", 365 - day); 
+  } 
+ } 
 }
