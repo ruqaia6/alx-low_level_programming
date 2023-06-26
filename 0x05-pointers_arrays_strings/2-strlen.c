d6934771c94548b19407f0814f697b061baa0191
ruqaia6 <rqyhm755@gmail.com>
2023-06-26 05:20:08 -0700

    lll

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -12,7 +13,7 @@ void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-		putchar(*str + 0);
+		_putchar(*str + 0);
 		str++;
 	}
 	_putchar('\n');
