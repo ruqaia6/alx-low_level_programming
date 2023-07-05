75c3b188553b24037e020e575c6f89de7c7f0aff
ruqaia6 <rqyhm755@gmail.com>
2023-07-05 15:21:35 -0700

    dd

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,32 +9,39 @@
 
 int _sqrt_recursion(int n)
 {
-    if (n < 0)
-    {
-        return (-1);
-    }
-    else if (n == 0 || n == 1)
-    {
-        return (n);
-    }
-    else
-    {
-        return (_sqrt_helper(n, 1));
-    }
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		return (_sqrt_helper(n, 1));
+	}
+
+	/**
+	 * square - find square root 
+	 * @n: int ti find square root
+	 * @val: square root
+	 * Return: int
+	 */
 }
 
 int _sqrt_helper(int n, int i)
 {
-    if (i * i == n)
-    {
-        return (i);
-    }
-    else if (i * i > n)
-    {
-        return (-1);
-    }
-    else
-    {
-        return (_sqrt_helper(n, i + 1));
-    }
+	if (i * i == n)
+	{
+		return (i);
+	}
+	else if (i * i > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (_sqrt_helper(n, i + 1));
+	}
 }
