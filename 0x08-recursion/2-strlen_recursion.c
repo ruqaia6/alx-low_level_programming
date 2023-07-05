8581ad86b609de8a9ba566601942571acad4a6a2
ruqaia6 <rqyhm755@gmail.com>
2023-07-05 14:39:33 -0700

    gg

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns to the length of a string
+ * @s: pointer to a string
+ * Return: int
+ */
+#include "main.h"
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	return (_strlen_recursion(s + 1) + 1);
+}
